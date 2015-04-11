b3a871f8a26100905624a33bd6d1f2ecad898115
Davanoffi <wyattliang@gmail.com>
2015-04-11 21:42:15 +0800

    验证码生成函数

---
+++ captcha_rest/tools.py
@@ -0,0 +1,34 @@
+import random
+import string
+
+from wheezy.captcha.image import captcha
+from wheezy.captcha.image import background
+from wheezy.captcha.image import curve
+from wheezy.captcha.image import noise
+from wheezy.captcha.image import smooth
+from wheezy.captcha.image import text
+from wheezy.captcha.image import offset
+from wheezy.captcha.image import rotate
+from wheezy.captcha.image import warp
+
+
+def getNewCaptha():
+    captcha_image = captcha(drawings=[
+       background(),
+       text(fonts=[
+           'fonts/CourierNew-Bold.ttf',
+           'fonts/LiberationMono-Bold.ttf'],
+        drawings=[
+            warp(),
+            rotate(),
+            offset()
+        ]),
+        curve(),
+        noise(),
+        smooth()
+    ])
+     
+    rand_string = random.sample(string.uppercase + string.digits, 4)
+    image = captcha_image(rand_string)
+
+    return rand_string, image
